5c59a747aa5ce86bef29e15dfbb6210ddddc5c42
Aleko Georgiev <algugone@gmail.com>
2025-02-22 00:12:11 +0200

    tasks 3, 4, 5

--- templates-and-anon-funcs/seminar.cpp
+++ templates-and-anon-funcs/seminar.cpp
@@ -23,6 +23,40 @@ auto composition(CompFuncFirst<T,U> f, CompFuncSecond<U,S> s) {
     };
 }
 
+//task 3
+template <typename T>
+auto repeat(T (*f)(T), unsigned n) {
+    auto function = f;
+
+    for (int i = 0; i < n; ++i) {
+        function = [function](T x) {
+            return function(x);
+        };
+    }
+
+    return function;
+}
+
+//task 4
+template <typename T>
+auto derivative(T (*f)(T)) {
+    constexpr double h = 1e-5;
+
+    return [f, h](T x) {
+        std::cout << h << std::endl;
+        return (f(x + h) - f(x - h)) / (2 * h);
+    };
+}
+
+//task 5
+template <typename T>
+void print(T* arr, unsigned n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     // task 1
     std::cout << std::boolalpha << is_fixed_point<int>([](int n){ return n * 5;}, 0) << '\n'; // -> true
@@ -31,6 +65,15 @@ int main() {
     // task 2
     std::cout << composition<int, int, int>([](int x){ return x + 1;}, 
                                         [](int x){ return x * 2;})(5) << '\n'; // -> 12
+
+    // task 4
+    std::cout << derivative<double>([](double x){ return x * x;})(5) << '\n'; // -> ~10
+    
+    // task 5
+    int arr[] = {1, 2, 3, 4};
+    print(arr, 4); // -> 1 2 3 4
+
+    // task 6
 
     return 0;
 }
