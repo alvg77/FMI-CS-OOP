30656f624f8907cafe260f30cf541bf530e84e69
Aleko Georgiev <61247544+alvg77@users.noreply.github.com>
2025-03-21 17:18:27 +0200

    Create LinkedList.hpp

---
+++ linked-list/practice/LinkedList.h
@@ -0,0 +1,36 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+template <typename T>
+struct Node {
+    T data;
+    Node* next;
+};
+
+template <typename T>
+class LinkedList {
+public:
+    LinkedList();
+    explicit LinkedList(T data);
+    explicit LinkedList(Node<T>* head);
+    LinkedList(const LinkedList& other);
+
+    LinkedList& operator=(const LinkedList& other);
+
+    ~LinkedList();
+
+    void insert(T data);
+    void insertAtStart(T data);
+    void remove(T data);
+    void reverse();
+    void printElements();
+
+private:
+    Node<T>* head;
+
+    Node<T>* getTail();
+};
+
+#include "LinkedList.tpp"
+
+#endif
