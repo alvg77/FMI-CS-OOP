1a2df12260c983a98f2ad1057e33c6e4ea7b86ca
Aleko Georgiev <61247544+alvg77@users.noreply.github.com>
2025-03-21 17:17:55 +0200

    Create main.cpp

---
+++ linked-list/practice/main.cpp
@@ -0,0 +1,19 @@
+#include "LinkedList.h"
+
+int main() {
+    auto l = LinkedList<int>{};
+
+    l.insert(10);
+    l.insertAtStart(5);
+    l.insert(24);
+    l.insertAtStart(123);
+    l.printElements();
+
+    l.remove(123);
+    l.printElements();
+
+    l.reverse();
+    l.printElements();
+
+    return 0;
+}
